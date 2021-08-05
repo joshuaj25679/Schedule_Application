37a1859d5458dd31a4332b929e58ff22c13ff362
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-08-05 11:36:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Schedule_Application
+ Project for Intro to Software Projects
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module prog.schedule_application {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens prog.schedule_application to javafx.fxml;
+    exports prog.schedule_application;
+}
