8907121805f9f9405b4415181db24928958f1f49
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-08-09 14:30:40 -0600

    Joke Submission

--- src/main/java/prog/schedule_application/HelloApplication.java
+++ src/main/java/prog/schedule_application/HelloApplication.java
@@ -12,11 +12,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Something!");
         stage.setScene(scene);
         stage.show();
-
-        //Testing Commits
     }
 
     public static void main(String[] args) {
