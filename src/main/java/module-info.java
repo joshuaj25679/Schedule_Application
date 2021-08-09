b66ba115ddbdd74b09f9af5796e8d07fb20cc445
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-08-09 14:48:24 -0600

    PDFTOTXT

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module prog.schedule_application {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires pdfbox.app;
 
     opens prog.schedule_application to javafx.fxml;
     exports prog.schedule_application;
--- src/main/java/prog/schedule_application/PDFtoTXT.java
+++ src/main/java/prog/schedule_application/PDFtoTXT.java
@@ -6,9 +6,18 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PDFtoTXT {
-    PDDocument document = PDDocument.load(new File("test.pdf"));
-if (!document.isEncrypted()) {
-        PDFTextStripper stripper = new PDFTextStripper();
-        String text = stripper.getText(document);
-        System.out.println("Text:" + text);
+
+    public static void test(){
+        try {
+            PDDocument document = PDDocument.class.cast(new File("test.pdf"));
+            if (!document.isEncrypted()) {
+                PDFTextStripper stripper = new PDFTextStripper();
+                String text = stripper.getText(document);
+                System.out.println("Text:" + text);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
