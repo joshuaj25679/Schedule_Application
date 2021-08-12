827b074e9cd986d5ac20d244ea5bc7fca9ccb70e
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-08-12 14:08:36 -0600

    Update Program.java

--- src/main/java/prog/schedule_application/controllers/Program.java
+++ src/main/java/prog/schedule_application/controllers/Program.java
@@ -1,5 +1,8 @@
 package prog.schedule_application.controllers;
+
 import prog.schedule_application.models.Course;
+import prog.schedule_application.models.Event;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,68 +16,80 @@ public class Program {
     private static Matcher m = null;
     private static String pathName = "src/main/files/test.pdf";
 
-    public static void promptForClass() {
-            String[] classes = new String[6];
-            for (int i = 0; i < 7; i++) {
-                classes[i] = promptForString("What is the Course Code (CSC180): ", false);
-            }
+    //TODO Make a method to output schedule nicely
+
+    public static ArrayList<Event> scheduleCreator(String[] userClasses, ArrayList<Course> courseList){
+        ArrayList<Event> schedule = new ArrayList<Event>();
+
+
+
+        return schedule;
+    }
+
+    public static String[] promptForClass() {
+        String[] userClasses = new String[6];
+        for (int i = 0; i < 6; i++) {
+            userClasses[i] = promptForString("What is the Course Code (CSC180): ", false);
         }
-        public static ArrayList<Course> buildCourses() {
-            ArrayList<Course> courseList = new ArrayList<>();
-            int counter = 0;
-            String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{1,2})[ ]([A-z\\s\\:\\-]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
-            PDFtoTXT.test("src/main/files/test.pdf");
-            for (String a : PDFtoTXT.pdfStrings) {
-                p = Pattern.compile(regex);
-                m = p.matcher(a);
-                if (m.find()) {
-                    LocalTime startTime = LocalTime.of(Integer.parseInt(m.group(5)), Integer.parseInt(m.group(6)));
-                    if (m.group(7).equals("PM")) {
-                        startTime = LocalTime.of(Integer.parseInt(m.group(5)) + 12, Integer.parseInt(m.group(6)));
-                    }
-                    LocalTime endTime = LocalTime.of(Integer.parseInt(m.group(8)), Integer.parseInt(m.group(9)));
-                    if (m.group(10).equals("PM")) {
-                        endTime = LocalTime.of(Integer.parseInt(m.group(8)) + 12, Integer.parseInt(m.group(9)));
-                    }
-                    String eventName = m.group(3);
-                    String days = m.group(11);
-                    String sectionCode = m.group(2);
-                    String courseCode = m.group(1);
-                    String roomNumber = m.group(12);
-                    Boolean isRequired = true;
-                    counter += 1;
-                    Course course = new Course(startTime, endTime, eventName, days, sectionCode, courseCode, roomNumber, isRequired);
-                    courseList.add(course);
-                    System.out.println(course);
+        return userClasses;
+    }
+
+    public static ArrayList<Course> buildCourses() {
+        ArrayList<Course> courseList = new ArrayList<>();
+        int counter = 0;
+        String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{1,2})[ ]([A-z\\s\\:\\-]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
+        PDFtoTXT.test("src/main/files/test.pdf");
+        for (String a : PDFtoTXT.pdfStrings) {
+            p = Pattern.compile(regex);
+            m = p.matcher(a);
+            if (m.find()) {
+                LocalTime startTime = LocalTime.of(Integer.parseInt(m.group(5)), Integer.parseInt(m.group(6)));
+                if (m.group(7).equals("PM")) {
+                    startTime = LocalTime.of(Integer.parseInt(m.group(5)) + 12, Integer.parseInt(m.group(6)));
                 }
-            }
-            return courseList;
-        }
-
-        public static String promptForString(String prompt, boolean allowBlank){
-            if (prompt == null || prompt.isBlank()) {
-                throw new IllegalArgumentException("The prompt cannot be null, empty, or just white space. prompt=" + prompt);
-            }
-
-            String input = null;
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            boolean inputIsInvalid = true;
-
-            do {
-                System.out.println(prompt);
-                try {
-                    input = br.readLine();
-                    inputIsInvalid = input == null || (!allowBlank && input.isBlank());
-
-                    if (inputIsInvalid) {
-                        System.out.println("Your input was invalid. Please, try again.");
-                    }
-                } catch (IOException ioe) {
-                    System.out.println("There was a problem and your input was not received. Please, try again.");
+                LocalTime endTime = LocalTime.of(Integer.parseInt(m.group(8)), Integer.parseInt(m.group(9)));
+                if (m.group(10).equals("PM")) {
+                    endTime = LocalTime.of(Integer.parseInt(m.group(8)) + 12, Integer.parseInt(m.group(9)));
                 }
-
-            } while (inputIsInvalid);
-
-            return input;
+                String eventName = m.group(3);
+                String days = m.group(11);
+                String sectionCode = m.group(2);
+                String courseCode = m.group(1);
+                String roomNumber = m.group(12);
+                Boolean isRequired = true;
+                counter += 1;
+                Course course = new Course(startTime, endTime, eventName, days, sectionCode, courseCode, roomNumber, isRequired);
+                courseList.add(course);
+                System.out.println(course);
+            }
         }
+        return courseList;
+    }
+
+    public static String promptForString(String prompt, boolean allowBlank) {
+        if (prompt == null || prompt.isBlank()) {
+            throw new IllegalArgumentException("The prompt cannot be null, empty, or just white space. prompt=" + prompt);
+        }
+
+        String input = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean inputIsInvalid = true;
+
+        do {
+            System.out.println(prompt);
+            try {
+                input = br.readLine();
+                inputIsInvalid = input == null || (!allowBlank && input.isBlank());
+
+                if (inputIsInvalid) {
+                    System.out.println("Your input was invalid. Please, try again.");
+                }
+            } catch (IOException ioe) {
+                System.out.println("There was a problem and your input was not received. Please, try again.");
+            }
+
+        } while (inputIsInvalid);
+
+        return input;
+    }
 }
