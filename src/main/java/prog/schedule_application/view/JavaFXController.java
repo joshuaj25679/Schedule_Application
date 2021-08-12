c8d14297556b2a5c945634a7f5d369e3c17017e9
Jesse3evans <78041192+Jesse3evans@users.noreply.github.com>
2021-08-12 14:26:58 -0600

    FMXL Button issues

--- src/main/java/prog/schedule_application/view/JavaFXController.java
+++ src/main/java/prog/schedule_application/view/JavaFXController.java
@@ -1,14 +1,41 @@
 package prog.schedule_application.view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class JavaFXController {
     @FXML
-    private Label welcomeText;
-
+    Button upload;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Here is Something Professor!");
+    Button home;
+
+    public void handleBtnUpload(ActionEvent actionEvent) throws IOException {
+
+
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("UploadScreen.fxml"));
+        Stage window = (Stage) this.upload.getScene().getWindow();
+        window.setScene(new Scene(root, 320, 240));
+        window.show();
+
     }
+
+    public void handleBtnHome(ActionEvent actionEvent) throws IOException {
+
+
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("homeScreen.fxml"));
+        Stage window = (Stage) this.home.getScene().getWindow();
+        window.setScene(new Scene(root, 320, 240));
+        window.show();
+
+    }
+
+
 }
