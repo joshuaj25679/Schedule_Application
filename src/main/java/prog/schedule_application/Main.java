cd75b781b6fd358437e20affab58a47ab4dd5ade
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-08-13 16:56:01 -0600

    Connected GUI to code back
    Made the button methods to do the needed operations of adding and displaying classes.

--- src/main/java/prog/schedule_application/Main.java
+++ src/main/java/prog/schedule_application/Main.java
@@ -21,11 +21,11 @@ public class Main extends Application {
         stage.show();
     }
 
-    private static String pathName = "src/main/files/test.pdf";
+
 
     public static void main(String[] args) {
-        String[] testUserCourse = new String[]{"CSC180", "ENG110", "MAT150", "DBT230", "PRO100"};
-        Program.scheduleCreator(testUserCourse, Program.buildCourses(pathName));
+        //String[] testUserCourse = new String[]{"CSC180", "ENG110", "MAT150", "DBT230", "PRO100"};
+        //Program.scheduleCreator(testUserCourse, Program.buildCourses(pathName));
         launch();
     }
 
