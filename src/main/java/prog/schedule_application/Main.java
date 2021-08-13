1fedeca03f3ecd632bcf00be61f79e68b1a67282
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-08-13 14:14:21 -0600

    Close App Button Added

--- src/main/java/prog/schedule_application/Main.java
+++ src/main/java/prog/schedule_application/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.controlsfx.control.spreadsheet.Grid;
 import prog.schedule_application.controllers.Program;
+import prog.schedule_application.models.Course;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        String[] testUserCourse = new String[]{"CSC180", "ENG110", "MAT150", "DBT230", "PRO100"};
+        ArrayList<Course> temp = Program.scheduleCreator(testUserCourse, Program.buildCourses(pathName));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homeScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Something!");
         stage.setScene(scene);
         stage.show();
@@ -21,9 +26,9 @@ public class Main extends Application {
     private static String pathName = "src/main/files/test.pdf";
 
     public static void main(String[] args) {
-        String[] testUserCourse = new String[]{"CSC180", "ENG110", "MAT150", "DBT230", "PRO100"};
-        Program.scheduleCreator(testUserCourse, Program.buildCourses(pathName));
-        //launch();
+//        String[] testUserCourse = new String[]{"CSC180", "ENG110", "MAT150", "DBT230", "PRO100"};
+//        Program.scheduleCreator(testUserCourse, Program.buildCourses(pathName));
+        launch();
     }
 
 }
--- src/main/java/prog/schedule_application/view/JavaFXController.java
+++ src/main/java/prog/schedule_application/view/JavaFXController.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.controlsfx.control.spreadsheet.Grid;
 import prog.schedule_application.Main;
 
 import java.io.IOException;
@@ -20,11 +21,14 @@ public class JavaFXController implements Initializable {
     Button upload;
     @FXML
     Button home;
+    @FXML
+    Button log;
+
 
     public void handleBtnUpload(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(Main.class.getResource("UploadScreen.fxml"));
         Stage window = (Stage) this.upload.getScene().getWindow();
-        window.setScene(new Scene(root, 320, 240));
+        window.setScene(new Scene(root, 800, 600));
         window.show();
 
     }
@@ -32,9 +36,13 @@ public class JavaFXController implements Initializable {
     public void handleBtnHome(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(Main.class.getResource("homeScreen.fxml"));
         Stage window = (Stage) this.home.getScene().getWindow();
-        window.setScene(new Scene(root, 320, 240));
+        window.setScene(new Scene(root, 800, 600));
         window.show();
+    }
 
+    public void handleCloseButtonAction(ActionEvent event) {
+        Stage stage = (Stage) log.getScene().getWindow();
+        stage.close();
     }
 
 
