ae2aa1d1e1dc22cec103bb416938f1b6db36d981
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-08-18 14:09:07 -0600

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/joshuaj25679/Schedule_Application into main"""
    This reverts commit 3aaeaaac1df17e5ce1ce2c04bc6f8ea3cf493e1d.

--- src/main/java/prog/schedule_application/Main.java
+++ src/main/java/prog/schedule_application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fileExplorer.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Something!");
         stage.setScene(scene);
         stage.show();
