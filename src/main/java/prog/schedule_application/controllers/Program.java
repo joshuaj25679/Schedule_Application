3921629dd0f20df68c16602fe50d240f38fde87c
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-08-24 14:05:21 -0600

    COMBOBOX VALUES ADDED

--- src/main/java/prog/schedule_application/controllers/Program.java
+++ src/main/java/prog/schedule_application/controllers/Program.java
@@ -11,6 +11,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Program {
     @FXML
