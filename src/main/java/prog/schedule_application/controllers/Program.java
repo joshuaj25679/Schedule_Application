de54bb9cf0dd71255adedd31db4ec2b5fd9ab040
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-08-25 14:25:16 -0600

    Update Program.java

--- src/main/java/prog/schedule_application/controllers/Program.java
+++ src/main/java/prog/schedule_application/controllers/Program.java
@@ -27,6 +27,69 @@ public class Program {
     private static ArrayList<Course> userSchedule = new ArrayList<>();
     private static String pathName = String.valueOf(filePath);
 
+    public static ArrayList<Course> buildCourses(String path) {
+        ArrayList<Course> courses = new ArrayList<>();
+        int counter = 0;
+        String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{0,2}[ ])?([A-z\\s\\:\\-]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
+        String sprintRegex = "^(Sprint 2)";
+        PDFtoTXT.test(path);
+        for (String a : PDFtoTXT.pdfStrings) {
+            p = Pattern.compile(regex);
+            m = p.matcher(a);
+            if (m.find()) {
+                LocalTime startTime = LocalTime.of(Integer.parseInt(m.group(5)), Integer.parseInt(m.group(6)));
+                if (m.group(7).equals("PM")) {
+                    startTime = LocalTime.of(Integer.parseInt(m.group(5)) + 12, Integer.parseInt(m.group(6)));
+                }
+                LocalTime endTime = LocalTime.of(Integer.parseInt(m.group(8)), Integer.parseInt(m.group(9)));
+                if (m.group(10).equals("PM")) {
+                    endTime = LocalTime.of(Integer.parseInt(m.group(8)) + 12, Integer.parseInt(m.group(9)));
+                }
+                String eventName = m.group(3);
+                String days = m.group(11);
+                String sectionCode = m.group(2) != null ? m.group(2) : "N/A";
+                String courseCode = m.group(1);
+                String roomNumber = m.group(12);
+                Boolean isRequired = true;
+                counter += 1;
+                Course course = new Course(startTime, endTime, eventName, days, sectionCode, courseCode, roomNumber);
+                if(isInList(course, courses)==false){
+                    courses.add(course);
+                }
+            }
+        }
+        return courses;
+    }
+
+    public static boolean isInList(Course course, ArrayList<Course> courses){
+        boolean check = false;
+        for(Course a : courses){
+            if(course.getCourseCode().equals(a.getCourseCode())
+                    && course.getSectionCode().equals(a.getSectionCode())
+                    && course.getRoomNumber().equals(a.getRoomNumber())
+                    && course.getEventName().equals(a.getEventName())
+                    && course.getDays().equals(a.getDays())){
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    public static boolean addToInputCourses(String courseToAdd){
+        String formattedInput = courseToAdd.toUpperCase();
+        formattedInput = formattedInput.replaceAll(" ", "");
+        String regex = "([A-Z]{3}[0-9]{3})";
+        p = Pattern.compile(regex);
+        m = p.matcher(formattedInput);
+        if(m.find()){
+            getInputCourses().add(formattedInput);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static ArrayList<Course> courseListCreator(int sprintTime, ArrayList<String> userClasses, ArrayList<Course> courseList) {
         ArrayList<Course> sprintCourses = new ArrayList<>();
 
@@ -84,7 +147,7 @@ public class Program {
                         scheduleOutprint.append(course);
                     }
                     else{
-                        System.out.println(course);
+                        //System.out.println(course);
                     }
                 }
                 break;
@@ -100,57 +163,8 @@ public class Program {
                 }
                 break;
         }
-
-
-        System.out.println(scheduleOutprint);
+        //System.out.println(scheduleOutprint);
         return scheduleOutprint.toString();
-    }
-
-    public static ArrayList<Course> buildCourses(String path) {
-        ArrayList<Course> courses = new ArrayList<>();
-        int counter = 0;
-        String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{0,2}[ ])?([A-z\\s\\:\\-]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
-        String sprintRegex = "^(Sprint 2)";
-        PDFtoTXT.test(path);
-        for (String a : PDFtoTXT.pdfStrings) {
-            p = Pattern.compile(regex);
-            m = p.matcher(a);
-            if (m.find()) {
-                LocalTime startTime = LocalTime.of(Integer.parseInt(m.group(5)), Integer.parseInt(m.group(6)));
-                if (m.group(7).equals("PM")) {
-                    startTime = LocalTime.of(Integer.parseInt(m.group(5)) + 12, Integer.parseInt(m.group(6)));
-                }
-                LocalTime endTime = LocalTime.of(Integer.parseInt(m.group(8)), Integer.parseInt(m.group(9)));
-                if (m.group(10).equals("PM")) {
-                    endTime = LocalTime.of(Integer.parseInt(m.group(8)) + 12, Integer.parseInt(m.group(9)));
-                }
-                String eventName = m.group(3);
-                String days = m.group(11);
-                String sectionCode = m.group(2) != null ? m.group(2) : "N/A";
-                String courseCode = m.group(1);
-                String roomNumber = m.group(12);
-                Boolean isRequired = true;
-                counter += 1;
-                Course course = new Course(startTime, endTime, eventName, days, sectionCode, courseCode, roomNumber);
-                if(isInList(course, courseList)==false){
-                    courseList.add(course);
-                }
-            }
-        }
-        return courseList;
-    }
-    public static boolean isInList(Course course, ArrayList<Course> courses){
-        boolean check = false;
-        for(Course a : courses){
-            if(course.getCourseCode().equals(a.getCourseCode())
-            && course.getSectionCode().equals(a.getSectionCode())
-            && course.getRoomNumber().equals(a.getRoomNumber())
-            && course.getEventName().equals(a.getEventName())
-            && course.getDays().equals(a.getDays())){
-                check = true;
-            }
-        }
-        return check;
     }
 
     public static String printCourseList(ArrayList<Course> courseList){
@@ -185,21 +199,6 @@ public class Program {
 
     public static void setInputCourses(ArrayList<String> inputCourses) {
         Program.inputCourses = inputCourses;
-    }
-
-    public static boolean addToInputCourses(String courseToAdd){
-        String formattedInput = courseToAdd.toUpperCase();
-        formattedInput = formattedInput.replaceAll(" ", "");
-        String regex = "([A-Z]{3}[0-9]{3})";
-        p = Pattern.compile(regex);
-        m = p.matcher(formattedInput);
-        if(m.find()){
-            getInputCourses().add(formattedInput);
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 
     public static ArrayList<Course> getUserCourseList(){
