af9e5fbc85cd3bd301b9011912a6aa6abf161729
Dougy1203 <78436877+Dougy1203@users.noreply.github.com>
2021-08-25 14:59:11 -0600

    regex update for Ampersand

--- src/main/java/prog/schedule_application/controllers/Program.java
+++ src/main/java/prog/schedule_application/controllers/Program.java
@@ -30,7 +30,7 @@ public class Program {
     public static ArrayList<Course> buildCourses(String path) {
         ArrayList<Course> courses = new ArrayList<>();
         int counter = 0;
-        String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{0,2}[ ])?([A-z0-9\\s\\:\\-]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
+        String regex = "^([A-Z]{3}[0-9]{3})[ ]([A-Z0-9]{0,2}[ ])?([A-z0-9\\s\\:\\-\\&]{1,})[ ]([0-9]{1})[ ]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[-]([0-9]{1,2})[:]([0-9]{1,2})[ ](AM|PM)[ ]([MTWHF]{1,5})[ ]([0-9]{3})";
         String sprintRegex = "^(Sprint 2)";
         PDFtoTXT.test(path);
         for (String a : PDFtoTXT.pdfStrings) {
