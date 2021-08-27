c3d182fad841ef4786e113976fd83ccea90f8fce
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-08-27 09:32:54 -0600

    Added ChangeScene Class

---
+++ src/main/java/prog/schedule_application/view/ChangeScene.java
@@ -0,0 +1,29 @@
+package prog.schedule_application.view;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class ChangeScene {
+
+    private static Stage stage;
+    private static Scene scene;
+
+    public static void changeScene(Event event, String strFXMLFileName) throws IOException {
+
+        URL url = new File("src/main/java/" + strFXMLFileName).toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
